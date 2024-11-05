f7d3866933d0b437e04d671d07f53e0784545a66
GibeNKozak <gibenkozak@gmail.com>
2024-11-05 01:53:28 +0100

    Wyzwanie 21 DNI: Zadanie domowe dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int number = 456785740;
+string numberToString = number.ToString();
+int[] digits = new int[10];
+
+foreach (char c in numberToString)
+{
+    int cyfra = int.Parse(c.ToString()); 
+    digits[cyfra]++; 
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Cyfra " + i + ": " + digits[i] + " razy");
+}
